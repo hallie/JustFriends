e265fc9ec5b2682872e94082bf02ba420749017f
Hallie Lomax <rachel.lomax@bison.howard.edu>
2015-11-27 21:12:11 -0800

    created a test helper to clean up places where code was duplicated

--- JustFriends/code/tests/places_test.py
+++ JustFriends/code/tests/places_test.py
@@ -1,15 +1,4 @@
-import sys, os, unittest
-
-# Absolure path to the current test file.
-TEST_FILE_PATH = os.path.abspath(__file__)
-# The path to the test directory.
-TEST_DIR = os.path.dirname(TEST_FILE_PATH)
-# The path to the directory of the main code.
-CODE_DIR = os.path.dirname(TEST_DIR)
-
-# Appending the path to the main code to the sys path.
-sys.path.append(CODE_DIR)
-
+import os, unittest, test_helper
 
 import places
 
